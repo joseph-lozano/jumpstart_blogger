02a052d97fe5d413eb204d20a61d7dd8ef5f8143
Joseph Lozano <lozanojoseph@gmail.com>
2015-07-16 14:58:00 -0700

    tagging

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,18 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(", ")
+  end
+
+  def tag_list=(tag_string)
+    tag_names = tag_string.split(",").map{|t| t.strip}.uniq
+    new_or_found_tags = tag_names.map{|name| Tag.find_or_create_by(name: name)}
+    self.tags = new_or_found_tags
+  end
+
 end
